a8c2d9a26fb6605d731db07994e4f68139d971e5
Rudy Leo <rudy.leo@protonmail.com>
2017-07-27 15:56:07 +0700

    add initial model

---
+++ ks1/database/migrations/2017_07_27_034159_create_materials_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('code',40)->unique();
+            $table->text('description');
+            $table->char('base_uom',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
